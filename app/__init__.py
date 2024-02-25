9521fe49478e8ac21f635ff5b13b11961d81d6c3
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2024-02-25 19:23:26 -0300

    fix: comentado 'SQLALCHEMY' no arquivo __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from mvc_flask import FlaskMVC
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
 
-db =SQLAlchemy()
+#db =SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "url_mySQL"
-
     FlaskMVC(app)
-    db.init_app(app)
-    Migrate(app, db)
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "url_mySQL"
+    # db.init_app(app)
+    # Migrate(app, db)
 
-    from app.models.post import Post
+    # from app.models.post import Post
 
     return app
 
