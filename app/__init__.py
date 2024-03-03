e96caad664791f52895c4592742e7816c10deb58
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2024-03-03 12:58:15 -0300

    feat: integração do DB com sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from mvc_flask import FlaskMVC
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-#db =SQLAlchemy()
+db =SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    FlaskMVC(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123456@localhost/car_care"
     
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "url_mySQL"
-    # db.init_app(app)
-    # Migrate(app, db)
+    FlaskMVC(app)
+    db.init_app(app)
+    Migrate(app, db)
 
-    # from app.models.post import Post
+    from app.models.post import Post
 
     return app
 
