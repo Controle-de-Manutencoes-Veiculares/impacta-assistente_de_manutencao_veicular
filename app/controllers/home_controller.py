277660f226c799fa3fc46b2ebb550f85c297144e
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2024-03-04 23:58:38 -0300

    criado consulta no DB

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -3,7 +3,7 @@ from app.models.post import Post
 class HomeController:
     def index(self):
         posts = Post.query.all()
-        return render_template('posts/index.html', posts=posts)
+        return render_template('posts/home/index.html', posts=posts)
     
     def show(self, name):
         
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
 from mvc_flask import Router
 
 Router.get('/', 'home#index')
+
+Router.all('posts', only='show')
