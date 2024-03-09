d4bc4cab8727a2336c6a5219cff496e8b1683726
Rafael <rafaelfsen@hotmail.com>
2024-03-09 12:43:44 -0300

    corrigido dados BD e iniciado rota Add Carro

--- app/controllers/carro_controller.py
+++ app/controllers/carro_controller.py
@@ -1,25 +1,39 @@
-from flask import render_template, redirect,request
+from flask import render_template, redirect, request
 from app.models.Carcare import Veiculo
+from app import db
+from app.controllers.home_controller import HomeController
 
 
-class CarroController: 
+class CarroController:
+
     def form_carro(self):
         return render_template('posts/home/form_carro.html')
 
-    def add_carro(self):
+    def add_carro():
+        
         # coletar dados do form via post
-        apelido = request.form.get("apelido-carro")
+        print(request.form.get("id_usuario-carro"))
+
+        id_usuario = request.form.get("id_usuario-carro")
         cor = request.form.get("cor-carro")
         placa = request.form.get("placa-carro")
         marca = request.form.get("marca-carro")
         km = request.form.get("km-carro")
         ano = request.form.get("ano-carro")
         modelo = request.form.get("modelo-carro")
-        print(apelido)
+        
+        # instanciar veiculo
+        carro = veiculo(id_usuario=id_usuario, cor=cor, placa=placa, marca=marca, km=km, ano=ano, modelo=modelo)
+        print(carro)
+        
         # regras para formulario incompleto
 
         # adicionar dados para tabela de veiculo (insert)
-        db.execute("INSERT INTO car_care.veiculo ( cor, placa, marca, km, ano, modelo) VALUES ( ?, ?, ?, ?, ?,?)", cor, placa, marca, km, ano, modelo)
+        db.session.add(carro)
+        db.session.commit()
+        db.session.close()
+        # db.execute("INSERT INTO car_care.veiculo ( cor, placa, marca, km, ano, modelo) VALUES ( ?, ?, ?, ?, ?,?)", cor, placa, marca, km, ano, modelo)
+        
         # verificar redirecionamento para rota index
-        return none
+        return redirect ('/')
 
